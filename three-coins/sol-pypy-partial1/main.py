c8c7e8c2f7f1260835857ce2630661d1ed3b297c
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-07-27 09:20:16 +0000

    test

---
+++ three-coins/sol-pypy-partial1/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env pypy
+
+n = int(input())
+a = list(map(int, input().split()))
+
+s = set()
+
+for x in a:
+    for y in a:
+        for z in a:
+            s.add(x + y + z)
+
+ans = 0
+for x in s:
+    ans ^= x
+
+print(ans)
