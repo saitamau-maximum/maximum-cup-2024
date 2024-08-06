b3468d2ab0a9db10784fd2943a9d911efa6f1096
a01sa01to <26807394+a01sa01to@users.noreply.github.com>
2024-08-06 22:18:55 +0900

    Revert "(test) import ACL"
    This reverts commit 20a7d561b36bf678a021f9513b8e97225e2299c9.

--- minimum-changes-on-bipartite-coloring/sol-cpy-ac/main.py
+++ minimum-changes-on-bipartite-coloring/sol-cpy-ac/main.py
@@ -1,7 +1,76 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 from sys import stdin
-from atcoder.dsu import DSU
+
+import typing
+
+
+# ac-library python
+class DSU:
+    """
+    Implement (union by size) + (path halving)
+
+    Reference:
+    Zvi Galil and Giuseppe F. Italiano,
+    Data structures and algorithms for disjoint set union problems
+    """
+
+    def __init__(self, n: int = 0) -> None:
+        self._n = n
+        self.parent_or_size = [-1] * n
+
+    def merge(self, a: int, b: int) -> int:
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        x = self.leader(a)
+        y = self.leader(b)
+
+        if x == y:
+            return x
+
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y = y, x
+
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+
+        return x
+
+    def same(self, a: int, b: int) -> bool:
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        return self.leader(a) == self.leader(b)
+
+    def leader(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        parent = self.parent_or_size[a]
+        while parent >= 0:
+            if self.parent_or_size[parent] < 0:
+                return parent
+            self.parent_or_size[a], a, parent = (
+                self.parent_or_size[parent],
+                self.parent_or_size[parent],
+                self.parent_or_size[self.parent_or_size[parent]],
+            )
+
+        return a
+
+    def size(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        return -self.parent_or_size[self.leader(a)]
+
+    def groups(self) -> typing.List[typing.List[int]]:
+        leader_buf = [self.leader(i) for i in range(self._n)]
+
+        result: typing.List[typing.List[int]] = [[] for _ in range(self._n)]
+        for i in range(self._n):
+            result[leader_buf[i]].append(i)
+
+        return list(filter(lambda r: r, result))
 
 
 def main():
