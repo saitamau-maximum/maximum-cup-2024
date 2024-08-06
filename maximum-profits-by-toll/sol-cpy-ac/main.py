eba1ff741bbb6060bacab74426197942d38ab7c8
a01sa01to <26807394+a01sa01to@users.noreply.github.com>
2024-08-06 19:24:22 +0900

    create cpy solutions

---
+++ maximum-profits-by-toll/sol-cpy-ac/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from sys import stdin
+import networkx as nx
+
+
+def main():
+    n, m = map(int, stdin.readline().split())
+    c = list(map(int, stdin.readline().split()))
+    G = nx.DiGraph()
+    G.add_nodes_from(range(n))
+    for _ in range(m):
+        u, v, h = map(int, stdin.readline().split())
+        G.add_edge(u - 1, v - 1, weight=h)
+
+    if not nx.is_directed_acyclic_graph(G):
+        print(-1)
+        return
+
+    ans = [0] * n
+    for v in nx.topological_sort(G):
+        for u in G.predecessors(v):
+            ans[v] = max(ans[v], ans[u] + G[u][v]["weight"])
+
+    ansval = sum(ans[i] * c[i] for i in range(n))
+    print(ansval)
+
+
+if __name__ == "__main__":
+    main()
---
+++ maximum-profits-by-toll/sol-cpy-partial1/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from sys import stdin
+import networkx as nx
+
+
+def main():
+    n, m = map(int, stdin.readline().split())
+    c = list(map(int, stdin.readline().split()))
+    G = nx.DiGraph()
+    G.add_nodes_from(range(n))
+    for _ in range(m):
+        u, v, h = map(int, stdin.readline().split())
+        G.add_edge(u - 1, v - 1, weight=h)
+
+    print(0 if nx.is_directed_acyclic_graph(G) else -1)
+
+
+if __name__ == "__main__":
+    main()
